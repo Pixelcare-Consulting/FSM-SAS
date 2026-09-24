278639df183dfaf0072387a7a8066710076d6af2
John <johncarlstienuy@gmail.com>
2026-09-24 15:55:11 +0800

    fix(Equipment): wrapped the style for the columns on the table.

--- pages/dashboard/tables/datatable-equipments.js
+++ pages/dashboard/tables/datatable-equipments.js
@@ -2,6 +2,7 @@ import React, { Fragment, useMemo, useState, useEffect } from 'react';
 import { Col, Row, Card } from 'react-bootstrap';
 import DataTable from 'react-data-table-component';
 import { Pagination } from 'sub-components';
+import styles from './datatable-equipments.module.css';
 
 const EquipmentsTable = ({ equipments, onSelectedRowsChange, initialSelected }) => {
 
@@ -31,7 +32,6 @@ const EquipmentsTable = ({ equipments, onSelectedRowsChange, initialSelected }) 
       style: {
         color: '#64748b',
         fontSize: '14px',
-        width: '50px',
       },
     },
     table: {
@@ -47,14 +47,14 @@ const EquipmentsTable = ({ equipments, onSelectedRowsChange, initialSelected }) 
   };
 
   const columns = [
-    { name: 'Item Code', selector: row => row.ItemCode, sortable: true },
-    { name: 'Item Name', selector: row => row.ItemName, sortable: true },
-    { name: 'Item Group', selector: row => row.ItemGroup, sortable: true },
-    { name: 'Model Series', selector: row => row.ModelSeries, sortable: true },
-    { name: 'Serial No', selector: row => row.SerialNo, sortable: true },
-    { name: 'Brand', selector: row => row.Brand, sortable: true },
-    { name: 'Notes', selector: row => row.Notes, sortable: true },
-    { name: 'Equipment Type', selector: row => row.EquipmentType, sortable: true },
+    { name: 'Item Code', selector: row => row.ItemCode, sortable: true, wrap: true },
+    { name: 'Item Name', selector: row => row.ItemName, sortable: true, wrap: true },
+    { name: 'Item Group', selector: row => row.ItemGroup, sortable: true, wrap: true },
+    { name: 'Model Series', selector: row => row.ModelSeries, sortable: true, wrap: true },
+    { name: 'Serial No', selector: row => row.SerialNo, sortable: true, wrap: true },
+    { name: 'Brand', selector: row => row.Brand, sortable: true, wrap: true },
+    { name: 'Notes', selector: row => row.Notes, sortable: true, wrap: true },
+    { name: 'Equipment Type', selector: row => row.EquipmentType, sortable: true, wrap: true },
     { 
       name: 'Location', 
       selector: row => buildServiceLocationAddress(row), 
@@ -76,8 +76,8 @@ const EquipmentsTable = ({ equipments, onSelectedRowsChange, initialSelected }) 
       wrap: true,
       grow: 1
     },
-    { name: 'Warranty Start Date', selector: row => row.WarrantyStartDate, sortable: true },
-    { name: 'Warranty End Date', selector: row => row.WarrantyEndDate, sortable: true },
+    { name: 'Warranty Start Date', selector: row => row.WarrantyStartDate, sortable: true, wrap: true },
+    { name: 'Warranty End Date', selector: row => row.WarrantyEndDate, sortable: true, wrap: true },
   
   ];
 
@@ -152,7 +152,7 @@ const EquipmentsTable = ({ equipments, onSelectedRowsChange, initialSelected }) 
               <h4 className="mb-1">List of Equipments</h4>
               <a>Select specific equipments for this job.</a>
             </Card.Header>
-            <Card.Body className='px-0'>
+            <Card.Body className={`px-0 ${styles.table}`}>
               <DataTable
                 customStyles={customStyles}
                 columns={columns}
